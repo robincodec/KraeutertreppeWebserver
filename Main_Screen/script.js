3306eb88e65eb23cb7a06f17ac9f87c414b47e01
Blju1013 <38442972+Blju1013@users.noreply.github.com>
2018-05-29 23:08:55 +0200

    Fortschritt!!!!

--- Main_Screen/script.js
+++ Main_Screen/script.js
@@ -28,13 +28,25 @@ var Pot3 = {
 var Pots = [Pot1, Pot2, Pot3];
 
 <!-- Functions -->
-function deleteFunction(){
-  var deleteconfirmation=false;
+function deleteFunction(element){
+  var potID = element.id.split("deletePot")[1];
   if(confirm("Wollen Sie die Pflanze wirklich aus dem System entfernen?")){
-    deleteconfirmation = true;
-    alert("Hello! I am an alert box!!");
-  } else {
-    deleteconfirmation = false;
+    Pots[potID-1].PflanzenName = "Keine Pflanze vorhanden";
+    Pots[potID-1].Ist_Feuchte = "0%";
+    Pots[potID-1].Soll_Feuchte = "0%";
+    Pots[potID-1].Bepflanzt = "false";
+    document.getElementById("plant" + potID).innerHTML = Pots[potID-1].PflanzenName;
+    document.getElementById("istFeuchte" + potID).innerHTML = Pots[potID-1].Ist_Feuchte;
+    document.getElementById("sollFeuchte" + potID).innerHTML = Pots[potID-1].Soll_Feuchte;
+  }
+}
+
+function loadIndex (){
+  for (i=1; i<=3; i++) {
+    document.getElementById("plant" + i).innerHTML = Pots[i-1].PflanzenName;
+    document.getElementById("istFeuchte" + i).innerHTML = Pots[i-1].Ist_Feuchte;
+    document.getElementById("sollFeuchte" + i).innerHTML = Pots[i-1].Soll_Feuchte;
+    document.getElementById("feuchteHistorie" + i).innerHTML = Pots[i-1].F_Historie;
   }
 }
 
